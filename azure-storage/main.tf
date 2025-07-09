6019612bfe1b23705ab0ab6ab6bf4eca85515f8b
Daniel Mena <dmenabarreto@gmail.com>
2025-07-09 16:33:10 -0300

    azure

--- azure-storage/main.tf
+++ azure-storage/main.tf
@@ -1,11 +1,6 @@
 terraform {
 
-    required_version = ">= 0.12"
     required_providers {
-        azurerm = {
-        source  = "hashicorp/azurerm"
-        version = "=3.0.0"
-        }
     }  
 }
 provider "azurerm" {
