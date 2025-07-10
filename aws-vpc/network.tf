6951dab88c61ceb490418060ae145cb020dfe54e
Daniel Mena <dmenabarreto@gmail.com>
2025-07-10 12:29:18 -0300

    vpc aws

--- aws-vpc/network.tf
+++ aws-vpc/network.tf
@@ -50,11 +50,10 @@ resource "aws_security_group" "security_group" {
     description = "SSH" 
     from_port        = 22
     to_port          = 22
-    protocol         = "-1"
+    protocol         = "tcp"
     cidr_blocks      = ["0.0.0.0/0"]
   
   }
-
 
     egress {
     from_port        = 0
