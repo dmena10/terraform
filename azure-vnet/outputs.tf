0ede8e16bfc8a108e166382a830107960d403b91
Daniel Mena <dmenabarreto@gmail.com>
2025-07-10 12:58:22 -0300

    vnet azure

---
+++ azure-vnet/outputs.tf
@@ -0,0 +1,9 @@
+output "subnet_id" {
+  description = "The ID of the created subnet."
+  value       = azurerm_subnet.subnet.id
+}
+
+output "security_group_id" {
+  description = "The ID of the created network security group."
+  value       = azurerm_network_security_group.security_group.id
+}
