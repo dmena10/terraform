3801f02bf5a69fb270d7c69a20cad7c65f15dea3
Daniel Mena <dmenabarreto@gmail.com>
2025-07-11 16:12:42 -0300

    key aws

--- aws-vm-vpc/vm.tf
+++ aws-vm-vpc/vm.tf
@@ -8,7 +8,7 @@ resource "aws_instance" "vm" {
   ami                     = "ami-020cba7c55df1f615"
   instance_type           = "t2.micro"
   key_name                = aws_key_pair.key.key_name
-  subnet_id = data.terraform_remote_state.vpc.outputs.subnet_id
+  subnet_id = data.terraform_remote_state.vpc.outputs.name
   vpc_security_group_ids  = [data.terraform_remote_state.vpc.outputs.security_group_id]
   associate_public_ip_address = true
 
