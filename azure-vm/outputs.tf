cec08bcc94487498cabc6d7753c07ab32a8d6bfe
Daniel Mena <dmenabarreto@gmail.com>
2025-07-14 11:45:22 -0300

    vnet azure

--- azure-vm/outputs.tf
+++ azure-vm/outputs.tf
@@ -2,3 +2,9 @@ output "vm_ip" {
   description = "The public IP address of the virtual machine"
   value       = azurerm_public_ip.public_ip.ip_address
 }
+
+output "subnet_id" {
+  description = "The ID of the subnet where the VM is deployed"
+  value       = data.terraform_remote_state.vnet.outputs.subnet_id
+  
+}
